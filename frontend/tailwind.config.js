c88a1e292d661a17cbe12cc7001036c83f6da434
matthew-pest <151239051+matthew-pest@users.noreply.github.com>
2024-02-09 13:46:03 -0600

    Edit button color to match footer gif

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = { 
 	content: [ "./src/**/*.{js,ts,jsx,tsx}" ], 
 	theme: { 
-		extend: {}, 
+		extend: {
+			colors: {
+				'custom-gray': '#d1d3d4',
+			  },
+		},
 	}, 
 	plugins: [], 
 }
